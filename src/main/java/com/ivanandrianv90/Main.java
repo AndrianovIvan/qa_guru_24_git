5e0be6bfb3090922cc7d266b7e267f8f4a2c9d1e
Ivan Andrianov <ivanandrianv90@gmail.com>
2023-12-29 19:40:23 +0300

    init commit3

--- src/main/java/com/ivanandrianv90/Main.java
+++ src/main/java/com/ivanandrianv90/Main.java
@@ -2,6 +2,6 @@ package com.ivanandrianv90;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello super qa_guru");
+        System.out.println("Hello super_qa_guru");
     }
 }
