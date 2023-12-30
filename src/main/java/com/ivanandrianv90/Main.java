76d89358372876652e0ff43bc0e05e1dc584f3a9
Ivan <154797633+AndrianovIvan@users.noreply.github.com>
2023-12-30 14:03:52 +0300

    Update Main.java

--- src/main/java/com/ivanandrianv90/Main.java
+++ src/main/java/com/ivanandrianv90/Main.java
@@ -2,6 +2,6 @@ package com.ivanandrianv90;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello supersuper_qa_guru");
+        System.out.println("Hello supersuper_qa_guru!!!!!!!");
     }
 }
