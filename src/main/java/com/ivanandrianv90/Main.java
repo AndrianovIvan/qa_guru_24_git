5bd9513a3a0ecfe032dbecf0c511cc1cce8f960d
Ivan Andrianov <ivanandrianv90@gmail.com>
2023-12-30 14:07:09 +0300

    сообщение изменено

--- src/main/java/com/ivanandrianv90/Main.java
+++ src/main/java/com/ivanandrianv90/Main.java
@@ -2,6 +2,10 @@ package com.ivanandrianv90;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello supersuper_qa_guru");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello supersuper_qa_guru";
     }
 }
